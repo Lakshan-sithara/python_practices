f932fe30c1bab6d279a8e3ef9289796aa0ec2e78
Lakshan Sithara <lakshansitharaabeysinghe@gmail.com>
2023-12-13 17:08:53 +0530

    different printing ways

---
+++ printing.py
@@ -0,0 +1,14 @@
+name = "sars_cov_2"
+disease = "covid19"
+
+print("the name of the virus is ", name)
+print("the name of virus is {}". format(name))
+
+print("{} is the name of the virus." . format(name))
+
+print("The name of virus is {} and it causes {}" . format(name,disease))
+
+print(f"the name of virus is {name} and it causes {disease}")
+
+#concadination
+print("the name of virus is"+" "+ name)

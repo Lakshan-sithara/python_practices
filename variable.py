1e88af5bf0750645c55948befb741047f2e3dc3d
Lakshan Sithara <lakshansitharaabeysinghe@gmail.com>
2023-12-13 13:22:03 +0530

    list, tupple , dictionary

---
+++ variable.py
@@ -0,0 +1,31 @@
+str1 = "helloo"
+num1 = 23
+#List (array) / collection of multi data type
+#we can chenge list data
+first_list = [str1 , "devops" , 47 , num1 , 1.5]
+print(first_list)
+
+#tupple / collection of multi datatype
+#we can't change date , but we can overwrite
+first_tupple = (str1 , "devops" , 47 , num1 , 1.5)
+print(first_tupple)
+
+#Dictionary collection of key value pairs (key:value)
+first_dictionary = {"name":"Lakshan" , "weight":75 ,"exercises":["boxing","dancing","joggin"]}
+print(first_dictionary)
+
+#variable types
+print("variable first_list is a: ",type(first_list))
+print("variable first_tupple is a: ",type(first_tupple))
+print("variable first_dictionary is a: ",type(first_dictionary))
+
+#booleance
+a = True
+b = False
+
+print(a)
+print(b)
+
+print(type(a))
+print(type(b))
+

61026bb808da5e6101894341e2f43a5235d308bd
Lakshan Sithara <lakshansitharaabeysinghe@gmail.com>
2023-12-17 16:12:37 +0530

    for and while loops

---
+++ loops.py
@@ -0,0 +1,38 @@
+# for loops
+"""PLANET = "earth"
+for i in PLANET:
+    print("value of i is now: ",i)"""
+
+"""VACCINES = ("moderna","pfizer","synophrme", "covaxin")
+
+for vac in VACCINES:
+    print(f"{vac} vaccine provides immunization against covid 19")
+
+VACCINES = ["moderna","pfizer","synophrme", "covaxin"]
+
+for vac in VACCINES:
+    print(f"{vac} vaccine provides immunization against covid 19")
+
+
+x = 0
+while x < 10:
+    print("value of x is now: ",x)
+    print("looping")
+    x+=1
+print("rest of the code")
+
+# loop 2 
+VACCINES = ["moderna","pfizer","synophrme", "covaxin"]
+for vac in VACCINES:
+    print("")
+    print("i would like to take a shot of ")
+    for i in vac:
+        print(i)
+"""
+import time
+x = 2
+while True:
+    print("value of x is now: ",x)
+    print("looping")
+    x*=2
+    time.sleep(2)
